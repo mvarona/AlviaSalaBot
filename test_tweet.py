b3e9c3969ffe1efc17efa1170242398339781f23
MarioVarona <hi@mariovarona.dev>
2023-06-17 19:11:05 +0200

    Build message

--- test_tweet.py
+++ test_tweet.py
@@ -1,6 +1,7 @@
 import unittest
 import tweet
 import os
+from datetime import date
 
 class TestTweet(unittest.TestCase):
 
@@ -13,6 +14,14 @@ class TestTweet(unittest.TestCase):
 		date2 = '2023-01-15'
 		result = tweet.days_between(date1, date2)
 		self.assertEqual(result, 14)
+
+	def test_GIVEN_date_WHEN_building_message_THEN_the_message_for_that_date_is_returned(self):
+		d0 = date(2020, 3, 14)
+		d1 = date.today()
+		days = (d1 - d0).days
+		expected_message = "Hace " + str(days) + " días @Renfe quitó el 50% de los Alvia entre Salamanca y Madrid.\nLlevamos 3 años aislados!!\nHartos de ser ciudadanos de 2ª!\nFirma https://www.change.org/p/tren-rápido-salamanca-ya\n@raquelsjimenez @mitmagob @alferma1 @CGCarbayo @A3Noticias @cyltv @elmundoes\n#EspañaVaciada #Renfe #TrenRápidoYa";
+		result = tweet.build_message()
+		self.assertEqual(result, expected_message)
 
 if __name__ == '__main__':
 	unittest.main()
--- tweet.py
+++ tweet.py
@@ -4,10 +4,16 @@ from dotenv import load_dotenv
 import os
 
 def days_between(date1, date2):
-	delta1 = datetime.strptime(date1, "%Y-%m-%d")
-	delta2 = datetime.strptime(date2, "%Y-%m-%d")
-	days = delta2 - delta1
+	d1 = datetime.strptime(date1, "%Y-%m-%d")
+	d2 = datetime.strptime(date2, "%Y-%m-%d")
+	days = d2 - d1
 	return days.days
+
+def build_message():
+	today = date.today().strftime("%Y-%m-%d")
+	days = days_between('2020-03-14', today)
+	message = "Hace " + str(days) + " días @Renfe quitó el 50% de los Alvia entre Salamanca y Madrid.\nLlevamos 3 años aislados!!\nHartos de ser ciudadanos de 2ª!\nFirma https://www.change.org/p/tren-rápido-salamanca-ya\n@raquelsjimenez @mitmagob @alferma1 @CGCarbayo @A3Noticias @cyltv @elmundoes\n#EspañaVaciada #Renfe #TrenRápidoYa"
+	return message
 
 def main():
 	load_dotenv()
@@ -15,3 +21,4 @@ def main():
 
 if __name__ == "__main__":
 	main()
+	
