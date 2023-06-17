33011738bfad53510db89d6554ed026398902b8f
MarioVarona <hi@mariovarona.dev>
2023-06-17 19:02:19 +0200

    Calculate days between dates

--- test_tweet.py
+++ test_tweet.py
@@ -8,5 +8,11 @@ class TestTweet(unittest.TestCase):
 		tweet.main()
 		self.assertEqual(os.environ.get('TEST_SECRET'), '1234')
 
+	def test_GIVEN_two_dates_WHEN_calculating_days_between_them_THEN_the_number_of_days_is_returned(self):
+		date1 = '2023-01-01'
+		date2 = '2023-01-15'
+		result = tweet.days_between(date1, date2)
+		self.assertEqual(result, 14)
+
 if __name__ == '__main__':
 	unittest.main()
--- tweet.py
+++ tweet.py
@@ -3,6 +3,12 @@ from datetime import date
 from dotenv import load_dotenv
 import os
 
+def days_between(date1, date2):
+	delta1 = datetime.strptime(date1, "%Y-%m-%d")
+	delta2 = datetime.strptime(date2, "%Y-%m-%d")
+	days = delta2 - delta1
+	return days.days
+
 def main():
 	load_dotenv()
 	#tweet()
