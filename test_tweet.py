8e5bd59ab27a18f88941b1c548c5710ae57dc7f5
MarioVarona <hi@mariovarona.dev>
2023-06-18 02:29:25 +0200

    Send request and end if successful

--- test_tweet.py
+++ test_tweet.py
@@ -2,16 +2,24 @@ import unittest
 import tweet
 import os
 from datetime import date
+from unittest import TestCase
+import mock
+from unittest.mock import patch
+import requests
+from requests_oauthlib import OAuth1
 
 class TestTweet(unittest.TestCase):
 
 	def test_GIVEN_env_secrets_WHEN_running_main_THEN_they_are_loaded(self):
+		f = tweet.make_request
+		tweet.make_request = mock.Mock(return_value="")
 		tweet.main()
-		self.assertEqual(os.environ.get('TEST_SECRET'), '1234')
+		self.assertEqual(os.environ.get("TEST_SECRET"), "1234")
+		tweet.make_request = f
 
 	def test_GIVEN_two_dates_WHEN_calculating_days_between_them_THEN_the_number_of_days_is_returned(self):
-		date1 = '2023-01-01'
-		date2 = '2023-01-15'
+		date1 = "2023-01-01"
+		date2 = "2023-01-15"
 		result = tweet.days_between(date1, date2)
 		self.assertEqual(result, 14)
 
@@ -23,5 +31,23 @@ class TestTweet(unittest.TestCase):
 		result = tweet.build_message()
 		self.assertEqual(result, expected_message)
 
-if __name__ == '__main__':
+	@mock.patch('tweet.requests.post')
+	def test_GIVEN_message_WHEN_building_request_AND_correct_response_THEN_the_request_succeeds(self, mock_post):
+		mock_post.return_value.status_code = 201
+		mock_post.return_value.json.return_value = {"response": "OK"}
+		result = tweet.make_request()
+		self.assertEqual(result, True)
+		self.assertEqual(mock_post.call_args[1]["json"], {'text': tweet.build_message()})
+		self.assertEqual(mock_post.call_args[1]["headers"], {'Content-Type': 'application/json'})
+		self.assertEqual(str(type(mock_post.call_args[1]["auth"])), "<class 'requests_oauthlib.oauth1_auth.OAuth1'>")
+
+def set_global_mocks():
+	tweet.get_url = mock.Mock(return_value="http://bmsalamanca.com/others/AlviaSalamancaBot/request.txt")
+	os.environ["CONSUMER_KEY"] = "CONSUMER_KEY"
+	os.environ["CONSUMER_SECRET"] = "CONSUMER_SECRET"
+	os.environ["ACCESS_TOKEN"] = "ACCESS_TOKEN"
+	os.environ["ACCESS_TOKEN_SECRET"] = "ACCESS_TOKEN_SECRET"
+
+if __name__ == "__main__":
+	set_global_mocks()
 	unittest.main()
--- tweet.py
+++ tweet.py
@@ -1,6 +1,9 @@
 from datetime import datetime
 from datetime import date
 from dotenv import load_dotenv
+import requests
+from requests_oauthlib import OAuth1
+import json
 import os
 
 def days_between(date1, date2):
@@ -11,14 +14,50 @@ def days_between(date1, date2):
 
 def build_message():
 	today = date.today().strftime("%Y-%m-%d")
-	days = days_between('2020-03-14', today)
+	days = days_between("2020-03-14", today)
 	message = "Hace " + str(days) + " días @Renfe quitó el 50% de los Alvia entre Salamanca y Madrid.\nLlevamos 3 años aislados!!\nHartos de ser ciudadanos de 2ª!\nFirma https://www.change.org/p/tren-rápido-salamanca-ya\n@raquelsjimenez @mitmagob @alferma1 @CGCarbayo @A3Noticias @cyltv @elmundoes\n#EspañaVaciada #Renfe #TrenRápidoYa"
 	return message
 
+def format_message(message):
+	return {"text": "{}".format(message)}
+
+def connect_to_oauth(consumer_key, consumer_secret, acccess_token, access_token_secret):
+	auth = OAuth1(consumer_key, consumer_secret, acccess_token, access_token_secret)
+	return auth
+
+def get_url():
+	return "https://api.twitter.com/2/tweets"
+
+def make_request():
+	consumer_key = os.environ.get("CONSUMER_KEY")
+	consumer_secret = os.environ.get("CONSUMER_SECRET")
+	access_token = os.environ.get("ACCESS_TOKEN")
+	access_token_secret = os.environ.get("ACCESS_TOKEN_SECRET")
+
+	message = build_message()
+	payload = format_message(message)
+	url = get_url()
+
+	auth = connect_to_oauth(
+		consumer_key, consumer_secret, access_token, access_token_secret
+	)
+
+	response = requests.post(
+		auth=auth, url=url, json=payload, headers={"Content-Type": "application/json"}
+	)
+
+	if 'content-type' in response.headers:
+		if 'json' in response.headers['content-type']:
+			debug_response = json.dumps(response.json())
+	else:
+		debug_response = response.text
+
+	print("Status: " + str(response.status_code) + ". Tweet: " + build_message())
+	return True
+
 def main():
 	load_dotenv()
-	#tweet()
+	make_request()
 
 if __name__ == "__main__":
 	main()
-	
